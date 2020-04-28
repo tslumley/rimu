71ccd92976dbf429a12c42db0f526e1f3abbde86
Annie Cohen <acohen3240@scrippscollege.edu>
2020-04-28 13:19:46 -0700

    updated functions

--- R/tidy.multiresp.R
+++ R/tidy.multiresp.R
@@ -1,22 +1,20 @@
 
-# as.td function
+library(tidyverse)
+library(vctrs)
+
+# as.td method
 
 as.td<-function(x,...) UseMethod("as.td",x)
+
+
+# as.td.td
+
 as.td.td<-function(x,...) x
 
-as.td.logical<-function(x,name,...){
-  if (!is.data.frame(x)){
-    x<-as.data.frame(x)
-    colnames(x)<-name
-  } else {
-    if (!missing(name))
-      colnames(x)<-name
-  }
-  class(x)<-"td"
-  x
-}
 
-as.td.list <- function(x,...,levels=NULL) {
+#as.td.list function
+
+as.td.list <- function(x, levels=NULL, name = NULL) {
   levs <- vec_c(unique(do.call(c,x)))
   if (!is.null(levels)) {
     if (any(xtra <- setdiff(levs,levels)))
@@ -31,35 +29,50 @@ as.td.list <- function(x,...,levels=NULL) {
     df[,i] <- vec_c(df[,i])
   }
   colnames(df)<-levs
-  class(df)<-"td"
+  if (!is.null(name)){
+    df <- as.td.logical(df, name = name)
+  }
+  else {
+    df <- as.td.logical(df, name = colnames(df))
+  }
   df
 }
 
+
+#as.td.logical function
+
+as.td.logical <- function(x, ..., name = NULL) {
+  if (sum(is.na(x))>0){
+  x <- replace_na(x, FALSE)
+  }
+  if (!is.matrix(x)){
+  x <- as.matrix(x)
+  colnames(x) <- name
+  }
+  else{
+    if (!is.null(name))
+      colnames(x) <- name
+  }
+  v <- c()
+for (i in 1:dim(x)[1]){
+v <- vec_c(v,paste(rep(colnames(x),matrix(as.numeric(x), ncol = dim(x)[2], nrow(x)[1])[i,]), collapse = "+"))
+}
+  class(v) <- "td"
+  v
+}
+
+
+#as.td.data.frame function
+
 as.td.data.frame<-function(x,...,na.rm=TRUE){
-  x <- as.data.frame(x > 0)
+  x <- as.matrix(x > 0)
   if (na.rm){
     x[is.na(x)]<-FALSE
   }
-  for(i in length(x)) {
-    x[,i] <- vec_c(x[,i])
-  }
-  class(x)<-"td"
+  x <- as.td.logical(x)
   x
 }
 
-as.td.logical<-function(x,name,...){
-  if (!is.data.frame(x)){
-    x<-as.data.frame(unclass(x))
-    colnames(x)<-name
-  } else {
-    if (!missing(name))
-      colnames(x)<-name
-  }
-  for(i in length(x)) {
-    x[,i] <- vec_c(x[,i])
-  }
-  class(x)<-"td"
-  x
-}
+
 
 
