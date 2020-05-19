6b90b3772e01a64eaa5bbdd5c467ac7a39c31b5e
Annie Cohen <acohen3240@scrippscollege.edu>
2020-05-18 20:42:23 -0700

    updated functions

--- R/tidy.multiresp.R
+++ R/tidy.multiresp.R
@@ -1,14 +1,13 @@
 
 library(tidyverse)
 library(vctrs)
+library(varhandle)
 
 # as.td method
 as.td<-function(x,...) UseMethod("as.td",x)
 
-
 # as.td.td
 as.td.td<-function(x,...) x
-
 
 #as.td.list function
 as.td.list <- function(x, levels=NULL, name = NULL) {
@@ -26,6 +25,7 @@ as.td.list <- function(x, levels=NULL, name = NULL) {
     df[,i] <- vec_c(df[,i])
   }
   colnames(df)<-levs
+  df <- as.matrix(df)
   if (!is.null(name)){
     df <- as.td.logical(df, name = name)
   }
@@ -36,38 +36,35 @@ as.td.list <- function(x, levels=NULL, name = NULL) {
 }
 
 
-#as.td.logical function
-as.td.logical <- function(x, ..., name = NULL) {
+#as.td.default ???
+
+#?????????
+as.td.factor<-function(){
+  as.mr.factor(x)
+}
+
+
+as.td <-function(x, ..., name = NULL){
+  x <- unclass(as.mr(x, name = name))
+  for (i in 1:length(x)) {
+  vec_assert(x[i],logical())
+  }
   if (sum(is.na(x))>0){
-  x <- replace_na(x, FALSE)
+    x <- replace_na(x, FALSE)
   }
   if (!is.matrix(x)){
-  x <- as.matrix(x)
-  colnames(x) <- name
+    x <- as.matrix(x)
+    colnames(x) <- name
   }
   else{
-    if (!is.null(name))
+    if (!is.null(name)){
       colnames(x) <- name
+    }
   }
   v <- c()
   for (i in 1:dim(x)[1]){
     v <- vec_c(v,paste(rep(colnames(x),matrix(as.numeric(x), ncol = dim(x)[2], nrow(x)[1])[i,]), collapse = "+"))
   }
-  class(v) <- "td"
-  v
+  new_vctr(v, class = "td")
 }
-
-
-#as.td.data.frame function
-as.td.data.frame<-function(x,...,na.rm=TRUE){
-  m <- as.matrix(x > 0)
-  if (na.rm){
-    m[is.na(m)]<-FALSE
-  }
-  v <- as.td.logical(m)
-  v
-}
-
-
-
 
